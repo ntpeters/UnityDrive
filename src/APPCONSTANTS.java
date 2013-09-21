0f8cc31ffce8e791a9db384ad7c5c5490bf27953
ntpeters <ntpeters@mtu.edu>
2013-09-21 03:08:13 -0400

    Added class for app constants

---
+++ src/APPCONSTANTS.java
@@ -0,0 +1,11 @@
+/**
+ * This file contains any constants this application requires.
+ * Primarily application keys for accessing the various services.
+ */
+public class APPCONSTANTS {
+
+    public static class Dropbox {
+        public static final String APP_KEY = "a1boy1ymao44h8q";
+        public static final String APP_SECRET = "yfbvljacppmqx6t";
+    }
+}
