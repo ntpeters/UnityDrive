279ca3b47f5d2de470c4b5d6ac3d17ab1ca3571e
Eric Rinkus <ejrinkus@mtu.edu>
2013-09-22 03:07:21 -0400

    WORKING DRIVE SESSION BABY!!!!

--- src/base/sessions/GDSession.java
+++ src/base/sessions/GDSession.java
@@ -26,17 +26,44 @@ import java.net.URI;
 
 public class GDSession implements UDSession {
 
+    /**
+     * Client Id DONT CHANGE
+     */
     private static String CLIENT_ID = "304793868911-vokk592ddao58s9oqm8f6jhj2udm6po5.apps.googleusercontent.com";
+    /**
+     * Client Super Secret Code DONT CHANGE
+     */
     private static String CLIENT_SECRET = "pK_uV6wVb0M13cXCIgdYP9lQ";
+    /**
+     * Redirect URI DONT CHANGE
+     */
     private static String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
 
+    /**
+     * Google drive service object
+     */
     private static Drive service;
+    /**
+     * Credential object containing the user credentials for this session
+     */
     private static GoogleCredential credential;
+    /**
+     * Token response received back from the server
+     */
     private static GoogleTokenResponse response;
 
+    /**
+     * Contains information about the user and their storage space
+     */
     private static AccountInfo info;
+    /**
+     * The current user's id for this session
+     */
     private static String username;
 
+    /**
+     * Mapping of accepted file types to supported MIME types
+     */
     private String[][] mimeTypes = {{"xls","application/vnd.ms-excel"},
         {"xlsx","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
         {"xml","text/xml"},
@@ -65,16 +92,25 @@ public class GDSession implements UDSession {
         {"folder","application/vnd.google-apps.folder"}};
 
     @Override
-    public boolean authenticate(String userID) throws UDException {
+    /**
+     * Authenticates a session with the current service
+     *
+     * @param userID        The id of the user, such as username, for the current service
+     * @return              Refresh token for this session
+     * @throws UDException
+     */
+    public String authenticate(String userID) throws UDException {
         username = userID;
         HttpTransport httpTransport = new NetHttpTransport();
         JsonFactory jsonFactory = new JacksonFactory();
 
+        //Set up the authorization flow
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
                 httpTransport, jsonFactory, CLIENT_ID, CLIENT_SECRET, Arrays.asList(DriveScopes.DRIVE))
                 .setAccessType("online")
                 .setApprovalPrompt("auto").build();
 
+        //Connect to the authorization url so that the user can enter the authorization code
         String url = flow.newAuthorizationUrl().setRedirectUri(REDIRECT_URI).build();
         System.out.println("Please open the following URL in your browser then type the authorization code:");
         System.out.println("  " + url);
@@ -93,20 +129,28 @@ public class GDSession implements UDSession {
             throw new UDException( "Unable to read the authentication token", e );
         }
 
+        //Receive the response token
         try {
             response = flow.newTokenRequest(code).setRedirectUri(REDIRECT_URI).execute();
         } catch (IOException e) {
             throw new UDException( "Unable to authenticate", e );
         }
 
+        //Set up the user credentials
         credential = new GoogleCredential().setFromTokenResponse(response);
 
         //Create a new authorized API client
         service = new Drive.Builder(httpTransport, jsonFactory, credential).build();
-        return true;
+        return credential.getRefreshToken();
     }
 
     @Override
+    /**
+     * Gets the account info for the user logged into the current session of the current service
+     *
+     * @return              An AccountInfo object containing all relevant user information
+     * @throws UDException
+     */
     public AccountInfo getAccountInfo() {
         info = new AccountInfo();
         About about = null;
@@ -123,13 +167,21 @@ public class GDSession implements UDSession {
     }
 
     @Override
+    /**
+     * Builds a tree of the directory structure for the current service and session
+     *
+     * @return              The root node of the tree representing the directory structure
+     * @throws UDException
+     */
     public UFile getFileList() throws UDException {
+        //Get the list of files in Google Drive
         Drive.Files.List list = null;
         try {
             list = service.files().list();
         } catch (IOException e) {
             throw new UDException("Unable to access the files in Drive",e);
         }
+        //Exclude files in the trash
         list.setQ("trashed = false");
         FileList files = null;
         try {
@@ -137,10 +189,14 @@ public class GDSession implements UDSession {
         } catch (IOException e) {
             throw new UDException("Unable to retrieve the file list in Drive",e);
         }
+
+        //Initialize the root UFile (the hierarchy will be represented as a tree from this root)
         UFile root = new UFile();
         root.setName("root");
         root.setOrigin(username + "-GoogleDrive");
+        //This map allows us to properly place files under their parents
         Map<UFile,String> fileMap = new HashMap<UFile,String>();
+        //Initialize all UFiles and place the first level files in the tree
         for(File f : files.getItems()){
             UFile entry = new UFile();
             entry.setId(f.getId());
@@ -154,6 +210,7 @@ public class GDSession implements UDSession {
                 fileMap.put(entry,f.getParents().get(0).getId());
             }
         }
+        //Loop through remaining files and place them under their proper parents
         for(UFile u : fileMap.keySet()){
             boolean found = false;
             for(UFile p : root.getChildren()){
@@ -168,18 +225,55 @@ public class GDSession implements UDSession {
                 if(p.getId().equals(fileMap.get(u))){
                     p.addChild(u);
                     u.setParent(p);
+                    found = true;
                 }
             }
+            if(found) continue;
+            else
+                root.addChild(u);
         }
         return root;
     }
 
     @Override
+    /**
+     * Searches the files/folders of the current session for the searchString
+     * Any word containing the searchString is returned
+     *
+     * @param searchString  String to search for in all file/folder names
+     * @return               List of all files/folders matching the search
+     * @throws UDException
+     */
     public List<UFile> searchFiles(String searchString) {
-        return null;
+        try{
+            //Populate the file list
+            UFile root = getFileList();
+            List<UFile> list = new ArrayList<UFile>();
+            searchHelper(root, searchString, list);
+            return list;
+        } catch(UDException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void searchHelper(UFile u, String match, List<UFile> list){
+        if(u.getName().contains(match)){
+            list.add(u);
+        }
+        for(UFile c : u.getChildren()){
+            searchHelper(c,match,list);
+        }
     }
 
     @Override
+    /**
+     * Uploads a file to the current service
+     *
+     * @param filename      The name of the file to upload to the service
+     * @return              The file that was uploaded
+     * @throws UDException
+     */
     public UFile upload(String filename) {
         // File's metadata.
         File body = new File();
@@ -197,10 +291,6 @@ public class GDSession implements UDSession {
         FileContent mediaContent = new FileContent(body.getMimeType(), fileContent);
         try {
             File file = service.files().insert(body, mediaContent).execute();
-
-            // Uncomment the following line to print the File ID.
-            // System.out.println("File ID: %s" + file.getId());
-
             return null;
         } catch (IOException e) {
             System.out.println("An error occured: " + e);
@@ -209,8 +299,16 @@ public class GDSession implements UDSession {
     }
 
     @Override
+    /**
+     * Downloads a file to the current service
+     *
+     * @param fileID        The id of the file to download
+     * @return              The file that was downloaded
+     * @throws UDException
+     */
     public UFile download(String fileID) {
         try{
+            //Find the file
             File file = null;
             for(File f : service.files().list().execute().getItems()){
                 if(f.getId().equals(fileID)){
@@ -219,6 +317,7 @@ public class GDSession implements UDSession {
                 }
             }
 
+            //Download the file
             if (file.getDownloadUrl() != null && file.getDownloadUrl().length() > 0) {
                 HttpResponse resp =
                         service.getRequestFactory().buildGetRequest(new GenericUrl(file.getDownloadUrl()))
@@ -244,6 +343,11 @@ public class GDSession implements UDSession {
     }
 
     @Override
+    /**
+     * Gets the type of session that this is
+     *
+     *@return               The session type
+     */
     public String getSessionType() {
         return "Google Drive";
     }
